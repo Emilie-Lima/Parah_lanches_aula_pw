2b367a3b3159440c2d751d4e1751b027f3aa818a
Emilie Lima <emilie.lima.rodrigues@gmail.com>
2026-04-28 09:36:55 -0300

    Add files via upload

---
+++ Parah_Lanches/js/script.js
@@ -0,0 +1,16 @@
+function verificarLogin(){
+
+let login = document.getElementById("login").value;
+let senha = document.getElementById("senha").value;
+
+
+if(login === "admin" && senha == 123456){
+    alert("Acesso Liberado!");
+} else{
+    alert("Acesso Negado")
+}
+
+login = document.getElementById("login").value = '';
+senha = document.getElementById("senha").value = '';
+
+}
